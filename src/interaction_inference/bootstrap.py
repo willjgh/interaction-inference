9efef2914e65d470856e146a56f7910e452de42d
willjgh <williamjgh2000@gmail.com>
2024-12-23 16:46:17 +0000

    Module with code to bootstrap sample for confidence intervals

---
+++ src/interaction_inference/bootstrap.py
@@ -0,0 +1,276 @@
+'''
+Module to compute confidence intervals from data for use in optimization.
+
+Given a sample of pairs of counts from a pair of genes, e.g. scRNA-seq data, 
+use bootstrap resampling to compute confidence interval bounds on the joint
+and marginal distributions of the sample.
+'''
+
+# ------------------------------------------------
+# Dependencies
+# ------------------------------------------------
+
+import numpy as np
+import tqdm
+import matplotlib.pyplot as plt
+
+# ------------------------------------------------
+# Functions
+# ------------------------------------------------
+
+def bootstrap(rng, sample, resamples, splits=1, beta=1.0, thresh_OB=10, threshM_OG=10, plot=False, printing=False):
+    '''
+    Compute confidence intervals of distribution of the sample.
+
+    Compute confidence intervals for the joint and marginal probabilities of the 
+    sample using the percentile bootstrap. Compute state space truncation using
+    a threshold on the number of samples per interval to improve coverage.
+
+    Args:
+        rng: numpy default rng object
+        sample: list of tuples (x1, x2) of counts per cell
+        resamples: number of bootstrap resamples (recommended = sample size)
+        splits: perform bootstrap resampling over multiple splits to avoid
+                memory issues (recommended = resamples / 100)
+        beta: per cell capture efficiency vector / single value for all cells
+        thresh_OB: observed truncation threshold
+        threshM_OB: marginal observed truncation threshold
+        plot: toggle plotting of confidence intervals and estimates
+        printing: toggle printing truncations
+
+    Returns:
+        A dictionary containing results
+
+        Sample information:
+
+        'samples': original sample used
+        'sample_counts': occurances of each state pair in the original sample
+        'sample_counts_x1': occurances of each state in the original sample (gene 1)
+        'sample_counts_x2': occurances of each state in the original sample (gene 2)
+
+        Confidence intervals:
+    
+        'joint': (2, _, _) numpy array of CI bounds on joint distribution
+        'x1': (2, _) numpy array of CI bounds on marginal distribution (gene 1)
+        'x2': (2, _) numpy array of CI bounds on marginal distribution (gene 2)
+
+        Truncation information
+
+        'min_x1_OB', 'max_x1_OB', 'min_x2_OB', 'max_x2_OB': joint truncation
+        'minM_x1_OB', 'maxM_x1_OB': marginal truncation (gene 1)
+        'minM_x2_OB', 'maxM_x2_OB': marginal truncation (gene 2)
+        'thresh_flag': bool if joint state space was truncated
+        'thresh_flag_x1': bool if marginal state space was truncated (gene 1)
+        'thresh_flag_x2': bool if marginal state space was truncated (gene 2)
+    '''
+
+    # sample size
+    n = len(samples)
+
+    # simulate data
+    #data = simulation(params, n, beta, tmax, ts, plot=False, initial_state=(0, 0))
+
+    # extract obserbed data
+    #samples = data['OB']
+
+    # compute maximum x1 and x2 values
+    M, N = np.max(samples, axis=0)
+    M, N = int(M), int(N)
+
+    # map (x1, x2) pairs to integers: x2 + (N + 1) * x1
+    integer_samples = np.array([x[1] + (N + 1)*x[0] for x in samples], dtype='uint32')
+
+    # maxiumum of integer sample
+    D = (M + 1)*(N + 1) - 1
+
+    # number of bootstrap samples per split
+    BS_split = BS // splits
+
+    # setup count array
+    counts = np.empty((BS, M + 1, N + 1), dtype='uint32')
+
+    # BS bootstrap samples: split into 'splits' number of BS_split x n arrays
+    for split in tqdm.tqdm(range(splits)):
+
+        # BS_split bootstrap samples as BS_split x n array
+        bootstrap_split = rng.choice(integer_samples, size=(BS_split, n))
+
+        # offset row i by (D + 1)i
+        bootstrap_split += np.arange(BS_split, dtype='uint32')[:, None]*(D + 1)
+
+        # flatten, count occurances of each state and reshape, reversing map to give counts of each (x1, x2) pair
+        counts_split = np.bincount(bootstrap_split.ravel(), minlength=BS_split*(D + 1)).reshape(-1, M + 1, N + 1)
+
+        # add to counts
+        counts[(split * BS_split):((split + 1) * BS_split), :, :] = counts_split
+
+    # sum over columns / rows to give counts (/n) of each x1 / x2 state
+    x1_counts = counts.sum(axis=2)
+    x2_counts = counts.sum(axis=1)
+
+    # compute 2.5% and 97.5% quantiles for each p(x1, x2), p(x1) and p(x2)
+    bounds = np.quantile(counts, [0.025, 0.975], axis=0)
+    x1_bounds = np.quantile(x1_counts, [0.025, 0.975], axis=0)
+    x2_bounds = np.quantile(x2_counts, [0.025, 0.975], axis=0)
+
+    # scale to probability
+    bounds = bounds / n
+    x1_bounds = x1_bounds / n
+    x2_bounds = x2_bounds / n
+
+    # count occurances per (x1, x2) in the in original sample
+    sample_counts = np.bincount(integer_samples, minlength=D + 1).reshape(M + 1, N + 1)
+
+    # sum over columns / rows to give counts per x1 / x2 state
+    x1_sample_counts = sample_counts.sum(axis=1)
+    x2_sample_counts = sample_counts.sum(axis=0)
+
+    # set truncation bounds
+    min_x1_OB, max_x1_OB, min_x2_OB, max_x2_OB = M, 0, N, 0
+    minM_x1_OB, maxM_x1_OB = M, 0
+    minM_x2_OB, maxM_x2_OB = N, 0
+
+    # set flag for changes
+    thresh_flag = False
+    thresh_flag_x1 = False
+    thresh_flag_x2 = False
+
+    # replace CI's for states below threshold occurances by [0, 1] bounds
+    for x1 in range(M + 1):
+        for x2 in range(N + 1):
+            # below: replace
+            if sample_counts[x1, x2] < thresh_OB:
+                bounds[:, x1, x2] = [0.0, 1.0]
+            # above: update truncation
+            else:
+                # check if smaller than current min
+                if x1 < min_x1_OB:
+                    min_x1_OB = x1
+                    thresh_flag = True
+                if x2 < min_x2_OB:
+                    min_x2_OB = x2
+                    thresh_flag = True
+                # check if larger than current max
+                if x1 > max_x1_OB:
+                    max_x1_OB = x1
+                    thresh_flag = True
+                if x2 > max_x2_OB:
+                    max_x2_OB = x2
+                    thresh_flag = True
+
+    for x1 in range(M + 1):
+        # below: replace
+        if x1_sample_counts[x1] < thresh_OB:
+            x1_bounds[:, x1] = [0.0, 1.0]
+        # above: update truncation
+        else:
+            # check if smaller than current min
+            if x1 < minM_x1_OB:
+                minM_x1_OB = x1
+                thresh_flag_x1 = True
+            # check if larger than current max
+            if x1 > maxM_x1_OB:
+                maxM_x1_OB = x1
+                thresh_flag_x1 = True
+
+    for x2 in range(N + 1):
+        # below: replace
+        if x2_sample_counts[x2] < thresh_OB:
+            x2_bounds[:, x2] = [0.0, 1.0]
+        # above: update truncation
+        else:
+            # check if smaller than current min
+            if x2 < minM_x2_OB:
+                minM_x2_OB = x2
+                thresh_flag_x2 = True
+            # check if larger than current max
+            if x2 > maxM_x2_OB:
+                maxM_x2_OB = x2
+                thresh_flag_x2 = True
+
+    # if no states were above threshold: default to max range, report
+    if not thresh_flag:
+        min_x1_OB, max_x1_OB, min_x2_OB, max_x2_OB = 0, M, 0, N
+    if not thresh_flag_x1:
+        minM_x1_OB, maxM_x1_OB = 0, M
+    if not thresh_flag_x2:
+        minM_x2_OB, maxM_x2_OB = 0, N
+
+    # plotting
+    if plot:
+        fig, axs = plt.subplots(M + 1, N + 1, figsize=(10, 10))
+        fig.tight_layout()
+        for x1 in range(M + 1):
+            for x2 in range(N + 1):
+                # within truncation: green CI lines
+                if (x1 >= min_x1_OB) and (x2 >= min_x2_OB) and (x1 <= max_x1_OB) and (x2 <= max_x2_OB):
+                    color = "green"
+                else:
+                    color = "red"
+                axs[x1, x2].hist(counts[:, x1, x2] / n)
+                axs[x1, x2].set_title(f"p({x1}, {x2})")
+                axs[x1, x2].axvline(bounds[0, x1, x2], color=color)
+                axs[x1, x2].axvline(bounds[1, x1, x2], color=color)
+
+        plt.suptitle("X1 X2 Confidence Intervals")
+        plt.show()
+
+        fig, axs = plt.subplots(1, M + 1, figsize=(10, 3))
+        fig.tight_layout()
+        for x1 in range(M + 1):
+            # within truncation: green CI lines
+            if (x1 >= minM_x1_OB) and (x1 <= maxM_x1_OB):
+                color = "green"
+            else:
+                color = "red"
+            axs[x1].hist(x1_counts[:, x1] / n)
+            axs[x1].set_title(f"p({x1})")
+            axs[x1].axvline(x1_bounds[0, x1], color=color)
+            axs[x1].axvline(x1_bounds[1, x1], color=color)
+
+        plt.suptitle("X1 Confidence Intervals")
+        plt.show()
+
+        fig, axs = plt.subplots(1, N + 1, figsize=(10, 3))
+        fig.tight_layout()
+        for x2 in range(N + 1):
+            # within truncation: green CI lines
+            if (x2 >= minM_x2_OB) and (x2 <= maxM_x2_OB):
+                color = "green"
+            else:
+                color = "red"
+            axs[x2].hist(x2_counts[:, x2] / n)
+            axs[x2].set_title(f"p({x2})")
+            axs[x2].axvline(x2_bounds[0, x2], color=color)
+            axs[x2].axvline(x2_bounds[1, x2], color=color)
+
+        plt.suptitle("X2 Confidence Intervals")
+        plt.show()
+
+    if printing:
+        print(f"Box truncation: [{min_x1_OB}, {max_x1_OB}] x [{min_x2_OB}, {max_x2_OB}]")
+        print(f"Marginal x1 truncation: [{minM_x1_OB}, {maxM_x1_OB}]")
+        print(f"Marginal x2 truncation: [{minM_x2_OB}, {maxM_x2_OB}]")
+
+    results =  {
+        'samples': samples,
+        'sample_counts': sample_counts,
+        'sample_counts_x1': x1_sample_counts,
+        'sample_counts_x2': x2_sample_counts,
+        'joint': bounds,
+        'x1': x1_bounds,
+        'x2': x2_bounds,
+        'min_x1_OB': min_x1_OB,
+        'max_x1_OB': max_x1_OB,
+        'min_x2_OB': min_x2_OB,
+        'max_x2_OB': max_x2_OB,
+        'minM_x1_OB': minM_x1_OB,
+        'maxM_x1_OB': maxM_x1_OB,
+        'minM_x2_OB': minM_x2_OB,
+        'maxM_x2_OB': maxM_x2_OB,
+        'thresh_flag': thresh_flag,
+        'thresh_flag_x1': thresh_flag_x1,
+        'thresh_flag_x2': thresh_flag_x2
+    }
+
+    return results
