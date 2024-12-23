d54a83fdd88f1cddd613fbbf9d25d521cf8e9dfd
willjgh <williamjgh2000@gmail.com>
2024-12-23 17:15:19 +0000

    Setup module for simulation of syntheic data

---
+++ src/interaction_inference/simulation.py
@@ -0,0 +1,8 @@
+'''
+Module to simulate synthetic data for testing inference methods.
+
+Simulate stochastic reaction network models using the gillespie algorithm to
+produce stationary samples emulating single-cell RNA sequencing observations.
+Simulate datatsets of samples from large numbers of genes by sampling model
+parameters from distributions fitted to match observed data.
+'''
