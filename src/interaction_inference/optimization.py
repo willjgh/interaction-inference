ebf0d0ce57bb9bb9041fe427a97a77b4a9bb2c80
willjgh <williamjgh2000@gmail.com>
2024-12-23 16:46:40 +0000

    Setup module for optimization code

---
+++ src/interaction_inference/optimization.py
@@ -0,0 +1,14 @@
+'''
+Module for optimization methods of inference
+Require confidence intervals and truncations to define optimization constraints
+'''
+
+# ------------------------------------------------
+# Dependencies
+# ------------------------------------------------
+
+
+
+# ------------------------------------------------
+# Functions
+# ------------------------------------------------
