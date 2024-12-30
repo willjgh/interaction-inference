1289e8b354b6ed3d8197ddef94512b1a98cca671
willjgh <williamjgh2000@gmail.com>
2024-12-30 21:27:38 +0000

    Dataset simulation method moved from module

--- src/interaction_inference/dataset.py
+++ src/interaction_inference/dataset.py
@@ -15,8 +15,8 @@ import numpy as np
 # ------------------------------------------------
 
 class Dataset():
-
     def __init__(self):
+        '''Initialise dataset settings'''
 
         # dataset iteself
         self.count_dataset = None
@@ -42,10 +42,9 @@ class Dataset():
 
     def load_dataset(self, beta, count_dataset_filename, param_dataset_filename=None):
         '''Load dataset from csv files: paramter and count data'''
-
-        # load datasets
         self.count_dataset = pd.read_csv(count_dataset_filename, index_col=0)
         if param_dataset_filename:
+            # parameter dataset only available for simulated data
             self.param_dataset = pd.read_csv(param_dataset_filename, index_col=0)
 
         # store shape and capture efficiency details
@@ -54,60 +53,142 @@ class Dataset():
 
     def store_dataset(self, count_dataset_filename, param_dataset_filename=None):
         '''Store dataset as csv files: parameter and count data'''
-
-        # store datasets
         self.count_dataset.to_csv(count_dataset_filename)
         if param_dataset_filename:
+            # parameter dataset only available for simulated data
             self.param_dataset.to_csv(param_dataset_filename)
-        
-
-    def simulate_dataset(self, cells, gene_pairs, beta, interaction, conditional, sig):
-        # call function from module
-        pass
 
     def pre_compute_truncation(self, size):
         # call compute truncation
         pass
 
+    def store_truncation(self):
+        # store as json
+        pass
 
-@dataclass
-class DatasetSettings:
-    '''Class to store dataset and associated settings.'''
-    dataset: pd.DataFrame = None
-    dataset_filename: str = None
-    cells: int = 1000
-    gene_pairs: int = 100
-    beta: np.array # could be Any to allow for float or array
-    interaction: bool = True
-    conditional: bool = False
-    sig: float = 0.5
-    thresh_OG: float = 10**-6
-    threshM_OG: float = 10**-6
-    truncations: dict = {}
-    trunactionsM: dict = {}
-    truncations_filename: str = None
-    truncationsM_filename: str = None
+    def load_truncation(self):
+        # load json
+        pass
 
-# ------------------------------------------------
-# Analysis settings class
-# ------------------------------------------------
+    def simulate_dataset(self, beta, gene_pairs, cells, interaction=True, conditional=False, sig=0.5, plot=False):
+        '''
+        Produce dataset of gene pairs' simulated parameters and samples.
 
-@dataclass
-class AnalysisSettings:
-    '''Class to store analysis settings and results.'''
-    resamples: int
-    splits: int
-    thresh_OB: int
-    threshM_OB: int
-    license_filename: str
-    constraint_settings: dict
-    time_limit: int = 300
-    silent: bool = True
-    K: int = 100
-    method: str
-    print_solution: bool = False
-    print_truncation: bool = False
-    MIPGap: float = 0.05
-    BestBdThresh: float = 0.0001
-    result_dict: dict
-    result_filename: str
+        Produce a dataset of pairs of genes where for each pair parameters of a
+        birth-death regulation model are simulated, and then a sample of size
+        'cells' is simulated from the model. Parameters are sampled from log-uniform 
+        distributions informed by single cell RNA sequencing experiments to produce
+        realistic data. The paramters and samples are returned in a dictionary of 2
+        pandas dataframes.
+
+        Args:
+            beta: per cell capture efficiency vector / single value
+            gene_pairs: number of gene pairs to simulate
+            cells: number of samples to simulate per gene pair
+            interaction: toggle if the interation parameter 'k_reg' is sampled (True)
+                        or set to 0 (False)
+            conditional: toggle if model parameters for each gene in the pair are
+                        sampled independently (False) or conditionally (True)
+            sig: standard deviation about common value for parameters of each gene
+                during conditional sampling 
+            plot: toggle plotting a scatter plot of the simulated parameters
+
+        Returns:
+            Nothing
+
+            Sets attributes self.count_dataset and self.param_dataset to
+            pandas dataframes of counts simulated per cell per gene-pair and
+            model parameters simulated per gene-pair. Sets attributes with 
+            simulation settings provided as arguments.
+        '''
+
+        # initialize random generator
+        rng = np.random.default_rng()
+
+        # dataframes
+        params_df = pd.DataFrame(index=[f"Gene-pair-{i}" for i in range(gene_pairs)], columns=['k_tx_1', 'k_tx_2', 'k_deg_1', 'k_deg_2', 'k_reg'])
+        counts_df = pd.DataFrame(index=[f"Gene-pair-{i}" for i in range(gene_pairs)], columns=[f"Cell-{j}" for j in range(cells)])
+
+        # for each gene
+        for i in tqdm.tqdm(range(gene_pairs)):
+
+            # Simulate reaction rate parameters 
+
+            # conditional sampling
+            if conditional:
+
+                # sample log-mean capture efficiency
+                log_mean = rng.uniform(-0.5, 1.5)
+
+                # sample from normal distribution about the log-mean
+                log_mean_1 = rng.normal(log_mean, sig)
+                log_mean_2 = rng.normal(log_mean, sig)
+
+                # sample degradation rates for each gene
+                log_k_deg_1 = rng.uniform(-1, 0)
+                log_k_deg_2 = rng.uniform(-1, 0)
+
+                # compute transcription rates using log-mean and deg rate
+                log_k_tx_beta_1 = log_mean_1 + log_k_deg_1
+                log_k_tx_beta_2 = log_mean_2 + log_k_deg_2
+
+                # sample interaction strength
+                if interaction: log_k_reg = rng.uniform(-1, 1)
+
+            # independent sampling
+            else:
+
+                # sample rates from log-uniform distribution for both genes
+                log_k_tx_beta_1 = rng.uniform(-1, 1.5)
+                log_k_tx_beta_2 = rng.uniform(-1, 1.5)
+                log_k_deg_1 = rng.uniform(-1, 0)
+                log_k_deg_2 = rng.uniform(-1, 0)
+                if interaction: log_k_reg = rng.uniform(-2, 2)
+
+            # exponentiate and scale
+            k_tx_1 = (10 ** log_k_tx_beta_1) / np.mean(beta)
+            k_tx_2 = (10 ** log_k_tx_beta_2) / np.mean(beta)
+            k_deg_1 = 10 ** log_k_deg_1
+            k_deg_2 = 10 ** log_k_deg_2
+            if interaction:
+                k_reg = 10 ** log_k_reg
+            else:
+                k_reg = 0
+
+            # store parameters
+            params_df.iloc[i] = [k_tx_1, k_tx_2, k_deg_1, k_deg_2, k_reg]
+
+            params = {
+                'k_tx_1': k_tx_1,
+                'k_tx_2': k_tx_2,
+                'k_deg_1': k_deg_1,
+                'k_deg_2': k_deg_2,
+                'k_reg': k_reg
+            }
+
+            # simulate sample from model
+            sample = gillespie(rng, params, cells, beta)
+
+            # store counts
+            counts_df.iloc[i] = sample['OB']
+
+        # plot
+        if plot:
+            plt.scatter(np.log10(params_df['k_tx_1'].astype(np.float64)) - np.log10(params_df['k_deg_1'].astype(np.float64)), np.log10(params_df['k_tx_2'].astype(np.float64)) - np.log10(params_df['k_deg_2'].astype(np.float64)))
+            plt.xlabel("log(k_tx_1 / k_deg_1)")
+            plt.ylabel("log(k_tx_2 / k_deg_2)")
+            plt.title("Scatter plot of gene-pair parameters")
+            plt.show()
+
+        # store datasets
+        self.count_dataset = counts_df
+        self.param_dataset = params_df
+
+        # store simulation settings
+        self.beta = beta
+        self.gene_pairs = gene_pairs
+        self.cells = cells
+        self.interaction = interaction
+        self.conditional = conditional
+        if conditional:
+            self.sig = sig
