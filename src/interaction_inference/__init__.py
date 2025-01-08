3ee798becc7e94667b01a0376e8931f8a7e2d6e3
willjgh <williamjgh2000@gmail.com>
2025-01-08 22:51:02 +0000

    Removed top level import of single result module function

--- src/interaction_inference/__init__.py
+++ src/interaction_inference/__init__.py
@@ -2,4 +2,3 @@ from .dataset import Dataset
 from .hypothesis import Hypothesis
 from .minimization import Minimization
 from .correlation import Correlation
-from .results import classification_performance
