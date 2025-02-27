144a3d407cb578cfa6a19cbd6d8cd7e1c000b602
willjgh <williamjgh2000@gmail.com>
2025-02-27 12:40:24 +0000

    Updated to call analysis on initialization

--- src/interaction_inference/correlation.py
+++ src/interaction_inference/correlation.py
@@ -12,65 +12,51 @@ import pandas as pd
 from ast import literal_eval
 
 # ------------------------------------------------
-# Hypothesis class
+# Correlation class
 # ------------------------------------------------
 
 class Correlation():
-    def __init__(self, method="pearson", alternative="less"):
+    def __init__(self, dataset, alternative="less", tqdm_disable=False, printing=True):
         '''Initialize analysis settings and result storage.'''
 
-        # analysis method settings
-        self.method = method
+        # store reference to dataset
+        self.dataset = dataset
+
+        # analysis settings
         self.alternative = alternative
 
-        # dataset used
-        self.dataset = None
+        # analyse dataset
+        self.analyse_dataset(tqdm_disable=tqdm_disable, printing=printing)
 
-        # results
-        self.result_dict = None
 
-    def analyse_dataset(self, dataset):
-        '''Analyse given dataset using method settings and store results.'''
-
-        # store reference to dataset used
-        self.dataset = dataset
+    def analyse_dataset(self, tqdm_disable=False, printing=True):
+        '''Analyse dataset using method settings and store results.'''
 
         # dict to store results
         solution_dict = {}
 
         # loop over dataset
-        for i in tqdm.tqdm(range(dataset.gene_pairs)):
+        for i in tqdm.tqdm(range(self.dataset.gene_pairs), disable=tqdm_disable):
 
             # select sample
-            sample = list(dataset.count_dataset.loc[f'Gene-pair-{i}'])
+            sample = list(self.dataset.count_dataset.loc[f'Gene-pair-{i}'])
 
             # convert string to tuple if neccessary (pandas reading csv to string)
             if type(sample[0]) == str:
                 sample = [literal_eval(count_pair) for count_pair in sample]
 
-            if self.method == "pearson":
+            # separate pairs into individual samples
+            x1_sample = [x[0] for x in sample]
+            x2_sample = [x[1] for x in sample]
 
-                # separate pairs into individual samples
-                x1_sample = [x[0] for x in sample]
-                x2_sample = [x[1] for x in sample]
+            # test
+            pearson = scipy.stats.pearsonr(x1_sample, x2_sample, alternative=self.alternative)
 
-                # test
-                pearson = scipy.stats.pearsonr(x1_sample, x2_sample, alternative=self.alternative)
+            # store result
+            solution_dict[i] = {'pvalue': float(pearson.pvalue), 'statistic': float(pearson.statistic)}
 
-                # store result
-                solution_dict[i] = {'pvalue': float(pearson.pvalue), 'statistic': float(pearson.statistic)}
-
-            elif self.method == "spearman":
-
-                # separate pairs into individual samples
-                x1_sample = [x[0] for x in sample]
-                x2_sample = [x[1] for x in sample]
-
-                # test
-                spearman = scipy.stats.spearmanr(x1_sample, x2_sample, alternative=self.alternative)
-
-                # store result
-                solution_dict[i] = {'pvalue': float(spearman.pvalue), 'statistic': float(spearman.statistic)}
+            if printing:
+                print(f"sample {i} p-value: {pearson.pvalue}")
 
         # store results
         self.result_dict = solution_dict
